fb6457d690fc45781f8f9d5c5a3fef914b98e8c0
Triskalimatu20 <triskalimatu@gmail.com>
2024-10-01 17:03:41 +0700

    Modifikasipercobaan1_19

--- Pemilihan2Percobaan1_19.java
+++ Pemilihan2Percobaan1_19.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
  public class Pemilihan2Percobaan1_19 {
     public static void main(String[] args){
-        Scanner input19 = new Scanner(System.in);
+        Scanner input17 = new Scanner(System.in);
         int tahun;
 
         System.out.print("Masukkan Tahun : ");
-        tahun = input19.nextInt();
+        tahun = input17.nextInt();
 
         if((tahun % 4) == 0) {
             if ((tahun % 100) != 0)
                 System.out.println("Tahun Kabisat");
+            else 
+                System.out.println("Bukan Tahun kabisat");
         } else
             System.out.println("Bukan Tahun Kabisat");
     }
- }
+}
